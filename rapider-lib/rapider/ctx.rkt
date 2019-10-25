0d0a0f75e8ba1d6300d5938b4ff21a9b452457f8
nuty <heizai3@gmail.com>
2019-10-25 23:41:13 +0800

    get-url

--- rapider-lib/rapider/ctx.rkt
+++ rapider-lib/rapider/ctx.rkt
@@ -15,15 +15,19 @@
 (struct status (version code text) #:transparent)
 (struct response (url status headers content) #:transparent)
 
+(define get-url
+  (λ (url-string [h (list)])
+    (log-info url-string)
+    (define-values (port header)
+      (get-pure-port/headers 
+        (string->url url-string) 
+        h
+        #:redirections 5
+        #:status? #t))
+    (define status (parse-status (get-status header)))
+    (define headers (headers->jsoneq (extract-all-fields header)))
+    (response url-string status headers (port->string port))))
 
-(define (get-url url-string)
-  (log-info url-string)
-  (define-values (port header)
-    (get-pure-port/headers (string->url url-string) #:redirections 5
-                           #:status? #t))
-  (define status (parse-status (get-status header)))
-  (define headers (headers->jsoneq (extract-all-fields header)))
-  (response url-string status headers (port->string port)))
 
 (define (headers->jsoneq header-dict)
   (for/hasheq ([hd header-dict])
--- rapider-lib/rapider/spider.rkt
+++ rapider-lib/rapider/spider.rkt
@@ -1,6 +1,6 @@
 #lang racket
 (require
-  "ctx.rkt")
+  "worker.rkt")
 
 
 (define spider%
