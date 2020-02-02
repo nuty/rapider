fc842724d3cc759b185cc7aba7f6c41b3bee8d41
rosso <heizai3@gmail.com>
2020-02-02 17:53:46 +0800

    bug fix

--- rapider-lib/info.rkt
+++ rapider-lib/info.rkt
@@ -1,7 +1,7 @@
 #lang info
 
 (define collection 'multi)
-(define deps '("base" "net-url" "sxml" "gregor" "html-parsing"))
+(define deps '("base" "net-url" "sxml" "gregor" "html-parsing" "sxml"))
 (define build-deps '())
 (define pkg-desc "implementation (no documentation) part of \"rapider\"")
 (define pkg-authors '(rosso))
